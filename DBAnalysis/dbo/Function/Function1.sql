d0f43b2affd841b10e211c859ee639199861a677
nahid_shamol@yahoo.com <nahid_shamol@yahoo.com>
2020-07-24 21:18:40 +0600

    Function Has been added

---
+++ DBAnalysis/dbo/Function/Function1.sql
@@ -0,0 +1,15 @@
+﻿
+Create  FUNCTION [dbo].[CalculateAge] (@DOB DATEtime2)
+RETURNS INT 
+AS 
+BEGIN
+	DECLARE @Age INT		
+		SET @Age = DATEDIFF( Month, @DOB, GETDATE())-
+		
+		CASE 	
+			WHEN (month(@DOB)>=month(GETDATE()))  AND (Day(@DOB)>Day(GETDATE())) THEN 1
+			
+	   ELSE 0
+	   END 
+		RETURN @Age 
+END
